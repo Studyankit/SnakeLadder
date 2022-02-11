a267c08eebfa1810485c330e386c44362a08ad11
StudyAnkit <ankitghosh2018@gmail.com>
2022-02-11 19:18:30 +0530

    Welcome to ludo game

---
+++ SnakeLadder/SnakeLadder/SnakeLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    class SnakeLadderSimulator
+    {
+       public static void main(Strig[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder simulator");
+        }
+    }
+}
+
+  
